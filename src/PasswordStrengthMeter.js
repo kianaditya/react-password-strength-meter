b7d70da6a929fecd36aefa39147348d071117451
Aditya Naik <naik.aditya@gmail.com>
2019-01-13 21:31:12 +0100

    Adds zxcvbn to test strength of password and displays it

--- src/PasswordStrengthMeter.js
+++ src/PasswordStrengthMeter.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+var zxcvbn = require("zxcvbn");
 
 export class PasswordStrengthMeter extends Component {
   render() {
     const { password } = this.props;
+    const testedResult = zxcvbn(password);
     return (
       <div>
         <div className="password-strength-meter">
-          <label className="password-strength-meter-label">{password}</label>
+          <label className="password-strength-meter-label">
+            {password} is your password and is a level {testedResult.score}{" "}
+            password
+          </label>
         </div>
       </div>
     );
