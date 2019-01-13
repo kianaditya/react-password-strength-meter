52eba974c0cd5fc306c900b5cc32b2829bbafb4d
Aditya Naik <naik.aditya@gmail.com>
2019-01-13 21:22:18 +0100

    Passes props from app to password-strength-meter and displays it

--- src/PasswordStrengthMeter.js
+++ src/PasswordStrengthMeter.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 
 export class PasswordStrengthMeter extends Component {
   render() {
+    const { password } = this.props;
     return (
       <div>
         <div className="password-strength-meter">
-          I'm a password strength meter
+          <label className="password-strength-meter-label">{password}</label>
         </div>
       </div>
     );
