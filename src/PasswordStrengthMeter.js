fb5de48a5ee1e645585450a752d0d6ab98a3da04
Aditya Naik <naik.aditya@gmail.com>
2019-01-13 21:14:10 +0100

    Adds password strength meter component

---
+++ src/PasswordStrengthMeter.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class PasswordStrengthMeter extends Component {
+  render() {
+    return (
+      <div>
+        <div className="password-strength-meter">
+          I'm a password strength meter
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PasswordStrengthMeter;
